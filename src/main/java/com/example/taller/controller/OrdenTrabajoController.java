9539e610fec17df67101a080687cf88a067932c7
NEORIS\p-ddelaflor <ddelaflorgp@gmail.com>
2024-10-08 15:13:00 +0200

    Añadir la funcionalidad para borrar ordenes

--- src/main/java/com/example/taller/controller/OrdenTrabajoController.java
+++ src/main/java/com/example/taller/controller/OrdenTrabajoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -91,4 +92,9 @@ public class OrdenTrabajoController {
         return ResponseEntity.ok(ordenModificada);
     }
 
+      @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarOrdenTrabajo(@PathVariable Long id) {
+        service.eliminarOrdenTrabajo(id);
+        return ResponseEntity.noContent().build(); // Devuelve 204 No Content
+    }
 }
