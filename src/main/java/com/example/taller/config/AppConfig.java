15fc2f11fe3c90d183fa50eae66437fac16772d2
NEORIS\p-ddelaflor <ddelaflorgp@gmail.com>
2024-10-09 20:45:31 +0200

    creo un server web para usar la api

--- src/main/java/com/example/taller/config/AppConfig.java
+++ src/main/java/com/example/taller/config/AppConfig.java
@@ -1,8 +1,10 @@
 package com.example.taller.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 //set DB_PASSWORD = root en command prompt
 }
